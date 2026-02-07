3d28a02112949b2017b21c9441bafb9050366ddd
Nicole <uwunicole.aquino@gmail.com>
2026-02-07 19:30:31 -0300

    componente produto sem style

---
+++ src/components/Forms.vue
@@ -0,0 +1,16 @@
+<script setup>
+input_form = getElementById(submeteicion)
+
+submeteicion.addEventListener("click", function(){
+    document.getElementById(text).innerHTML = "Adicionar Nova Maquiagem"
+})
+</script>
+
+<style scoped>
+    template{
+        background-color: #D9D9D9;
+        width: 1019px;
+        height: 596px;
+    }
+
+</style>
---
+++ src/components/Fundos.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2>Feito por: IFRN/CNAT/DIATINF/Juliana Vianna e Nicole de Aquino </h2>
+    <address>Rua dos Bobos, 0 - Casa Muito Engraçada - RN. CEP: 0 | CNPJ: 0</address>
+</template>
+
+<style scoped>
+
+</style>
